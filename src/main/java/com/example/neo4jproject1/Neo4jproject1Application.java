b7bca8d2af18ae4375098d7dd25628f58e918d1a
crenacernegro <crenacernegro@gmail.com>
2024-01-27 13:21:35 -0500

    first commit

---
+++ src/main/java/com/example/neo4jproject1/Neo4jproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.neo4jproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neo4jproject1/Neo4jproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neo4jproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
